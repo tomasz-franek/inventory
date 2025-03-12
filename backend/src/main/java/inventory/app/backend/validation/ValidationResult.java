28f0a1be44c70edb291d4b4095a23c901cd51939
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-03-12 12:29:50 +0100

    improve code coverage

--- backend/src/main/java/inventory/app/backend/validation/ValidationResult.java
+++ backend/src/main/java/inventory/app/backend/validation/ValidationResult.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Getter
@@ -27,41 +26,21 @@ public class ValidationResult {
         errors.add(new ValidationError(context, message));
     }
 
-    public void addError(ValidationError errorMessage) {
-        this.errors.add(errorMessage);
-    }
-
     public boolean isFailing() {
         return !errors.isEmpty();
     }
 
-    public <T extends RuntimeException> ValidationResult throwIfFailing(
-            Function<ValidationResult, T> exceptionSupplier) {
-        if (isFailing()) {
-            throw exceptionSupplier.apply(this);
-        }
-        return this;
-    }
 
     public record ValidationError(@JsonUnwrapped Context context, String message) {
-
     }
 
     public interface Context {
-
         static Supplier<Context> contextSupplier(Object object) {
             return () -> new StringContext(object.getClass().getSimpleName());
         }
-
     }
-
     public record FieldContext(Collection<String> fieldNames) implements Context {
-
     }
-
     public record StringContext(String text) implements Context {
-
     }
-
-
 }
