26b2548da146b340118845cf7d331df71e2b7c67
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-03-18 10:50:11 +0100

    fix download-file.component test

--- frontend/src/app/download-file/download-file.component.spec.ts
+++ frontend/src/app/download-file/download-file.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DownloadFileComponent } from './download-file.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('DownloadFileComponent', () => {
   let component: DownloadFileComponent;
@@ -9,6 +10,7 @@ describe('DownloadFileComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DownloadFileComponent, TranslateModule.forRoot()],
+      providers: [provideMockStore({})],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DownloadFileComponent);
